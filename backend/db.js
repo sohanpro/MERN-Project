8e6cd50802dc779ea50c82a9b8269f1b529ff12b
sohanpro <shsohan47@gmail.com>
2023-08-08 18:07:06 +0600

    fetch data nodemon many more feature

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://admin:admin@cluster0.0ml6zvi.mongodb.net/foodDetails?retryWrites=true&w=majority';
+
+async function connection() {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected successfully to MongoDB hi');
+    const FetchData = await mongoose.connection.db.collection("admin");
+    const data = await FetchData.find({}).toArray()
+    console.log()
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+module.exports = connection;
